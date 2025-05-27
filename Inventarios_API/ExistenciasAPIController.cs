b421ee363719b80c89833a7d3769fa14afe1fe6f
esquivelx17 <151577929+esquivelx17@users.noreply.github.com>
2025-05-26 20:25:09 -0600

    api

---
+++ Inventarios_API/ExistenciasAPIController.cs
@@ -0,0 +1,36 @@
+﻿using InventariosCore.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Inv
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExistenciasAPIController : ControllerBase
+    {
+        private readonly ProductosController _productosController;
+        private readonly ILogger<ExistenciasAPIController> _logger;
+
+        public ExistenciasAPIController(ProductosController productosController, ILogger<ExistenciasAPIController> logger)
+        {
+            _productosController = productosController;
+            _logger = logger;
+        }
+
+        [HttpGet("list_existencias")]
+        public IActionResult GetExistencias([FromQuery] string clave = null)
+        {
+            try
+            {
+                var existencias = _productosController.ObtenerProductosParaAPI(clave);
+
+                return Ok(existencias);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error al obtener existencias");
+                return StatusCode(500, "Error interno del servidor: " + ex.Message);
+            }
+        }
+
+    }
+}
