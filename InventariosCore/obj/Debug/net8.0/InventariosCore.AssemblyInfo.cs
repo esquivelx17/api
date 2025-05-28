7d8209e6aa12b99b70aec8c5f5d71898ec00d2e0
José Emilio Esquivel Narváez <L21281036@toluca.tecnm.mx>
2025-05-28 13:59:20 -0600

    mov

--- InventariosCore/obj/Debug/net8.0/InventariosCore.AssemblyInfo.cs
+++ InventariosCore/obj/Debug/net8.0/InventariosCore.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventariosCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be9b222f487c28c91798f6c89340058f592e6a07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505ec15750f8462eea4fc92e42a55f2c1fcba363")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventariosCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventariosCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
